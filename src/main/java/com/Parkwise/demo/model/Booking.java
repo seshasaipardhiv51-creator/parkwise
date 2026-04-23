bcef1a532587309a1bc2b31c2f9cff937afb8403
seshasaipardhiv51-creator <seshasaipardhiv51@gmail.com>
2026-04-23 10:59:24 +0530

    Update Booking.java

--- src/main/java/com/Parkwise/demo/model/Booking.java
+++ src/main/java/com/Parkwise/demo/model/Booking.java
@@ -1,8 +1,9 @@
-package com.parkwise.model;
+package com.parkwise.demo.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
+
 
 @Entity
 @Data
@@ -19,4 +20,23 @@ public class Booking {
     private String phoneNumber;
     private LocalDateTime fromTime;
     private LocalDateTime toTime;
+
+    // getters & setters
+    public Long getId() { return id; }
+
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public String getSlot() { return slot; }
+    public void setSlot(String slot) { this.slot = slot; }
+
+    public String getCarNumber() { return carNumber; }
+    public void setCarNumber(String carNumber) { this.carNumber = carNumber; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
 }
