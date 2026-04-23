37fe3b344944904cc60e1406d40b4faa505e61a3
seshasaipardhiv51-creator <seshasaipardhiv51@gmail.com>
2026-04-23 11:02:22 +0530

    Update BookingRepository.java

--- src/main/java/com/Parkwise/demo/repository/BookingRepository.java
+++ src/main/java/com/Parkwise/demo/repository/BookingRepository.java
@@ -1,11 +1,10 @@
-package com.parkwise.repository;
-
-import com.parkwise.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
+package com.parkwise.demo.repository;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.parkwise.demo.model.Booking;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-
+    
     List<Booking> findByToTimeBefore(LocalDateTime time);
 }
