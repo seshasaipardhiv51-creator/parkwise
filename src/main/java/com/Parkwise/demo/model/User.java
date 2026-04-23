b66761c807a4efa15d3073a3996e668e66eab1bb
seshasaipardhiv51-creator <seshasaipardhiv51@gmail.com>
2026-04-23 11:01:12 +0530

    Update User.java

--- src/main/java/com/Parkwise/demo/model/User.java
+++ src/main/java/com/Parkwise/demo/model/User.java
@@ -1,20 +1,47 @@
 package com.parkwise.model;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
-
 @Entity
 @Data
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
     private String password;
     private String role;
+}package com.parkwise.demo.model;
+import jakarta.persistence.*;
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+    public Long getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
